a669c42f301bc4f86220aab6cd6010170580f4e0
root <root@vmwork.myguest.virtualbox.org>
2023-04-07 12:59:19 -0700

    ML algorithm for scheduling

--- kernel/Scheduler.h
+++ kernel/Scheduler.h
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2009 Niek Linnenbank
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef __KERNEL_SCHEDULER_H
 #define __KERNEL_SCHEDULER_H
 #ifndef __ASSEMBLER__
@@ -89,8 +72,11 @@ class Scheduler
 
   private:
 
+    /** Number of queues */
+    static const int NUM_QUEUES = 5;
+
     /** Contains processes ready to run */
-    Queue<Process *, MAX_PROCS> m_queue;
+    Queue<Process *, MAX_PROCS> m_queues[NUM_QUEUES];
 };
 
 /**
